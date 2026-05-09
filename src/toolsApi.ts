743b234f8266314984059ec6d94cc9f8621d7228
Sebastian Hyland <st.hyland05@gmail.com>
2026-05-09 16:30:17 -0700

    feat: scaffold toolsApi utilities

--- src/toolsApi.ts
+++ src/toolsApi.ts
@@ -6,18 +6,26 @@ type Result<T> = T | Error;
 
 let _clientPromise: Promise<Result<ToolsClient>> | null = null;
 
+/**
+ * Public interface to a singleton `ToolsClient` interface.
+ *
+ * TODO: do we want to pull credentials from .env?
+ */
 export async function getToolsClient(): Promise<Result<ToolsClient>> {
     if (_clientPromise) return _clientPromise;
 
     _clientPromise = (async (): Promise<Result<ToolsClient>> => {
-        const { IGEM_TOOLS_USERNAME: user, IGEM_TOOLS_PASSWORD: pass } =
+        const { IGEM_TOOLS_USERNAME: username, IGEM_TOOLS_PASSWORD: password } =
             process.env;
 
-        if (!user || !pass) {
+        if (!username || !password) {
             return Error("Missing IGEM_TOOLS_ environment variables.");
         }
 
-        return await ToolsClient.withAuthentication(user, pass);
+        return await ToolsClient.withAuthentication({
+            username,
+            password,
+        });
     })();
 
     return _clientPromise;
@@ -27,6 +35,7 @@ class ToolsClient {
     private client: AxiosInstance;
 
     private constructor() {
+        // Internally holds a cookie store so we don't need to constantly re-authenticate our session
         const jar = new CookieJar();
         this.client = wrapper(
             axios.create({
@@ -37,10 +46,13 @@ class ToolsClient {
         );
     }
 
-    public static async withAuthentication(
-        username: string,
-        password: string,
-    ): Promise<Result<ToolsClient>> {
+    public static async withAuthentication({
+        username,
+        password,
+    }: {
+        username: string;
+        password: string;
+    }): Promise<Result<ToolsClient>> {
         const instance = new ToolsClient();
 
         const params = new URLSearchParams({
@@ -56,7 +68,31 @@ class ToolsClient {
         }
     }
 
-    public async upload() {
+    public async upload({
+        folderName,
+        fileName,
+        url,
+    }: {
+        folderName: string;
+        fileName: string;
+        url: string;
+    }): Promise<Result<null>> {
         // TODO
+        // This will probably act like `fileSystem.ts/download`, but instead of downloading locally
+        // it will stream data from `url` to the Tools API. You may wish to modularize the current
+        // `fileSystem.ts/download` implementation
+        return null;
+    }
+
+    public async alreadyUploaded({
+        folderName,
+        fileName,
+    }: {
+        folderName: string;
+        fileName: string;
+    }): Promise<Result<boolean>> {
+        // TODO
+        // Has it been already uploaded?
+        return false;
     }
 }
